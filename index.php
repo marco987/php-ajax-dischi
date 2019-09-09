ad6dac1c036ea8c50e9a0f78a42e450cb0e40095
marco987 <pacemarco87@gmail.com>
2019-09-09 16:22:36 +0200

    CSS ok

---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>php-ajax-dischi</title>
+    <!-- JS: JQUERY -->
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <!-- JS: MY SCRIPT -->
+    <script src="script.js" charset="utf-8"></script>
+    <!-- JS: HANDLEBARS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/handlebars.js/4.1.2/handlebars.min.js" charset="utf-8"></script>
+    <!-- TEMPLATE: MESSAGE MENU -->
+    <script id="item-template" type="text/x-handlebars-template">
+    </script>
+    <!-- CSS: MY STYLE -->
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <div class="container">
+      <header>
+        <img src="img/logo.png" alt="">
+      </header>
+      <main>
+        <div class="album">
+          <span><img src="img/album.png" alt=""></span>
+          <h4>The dark side of the moon</h4>
+          <p>Pink Floyd</p>
+          <h5>1973</h5>
+        </div>
+        <div class="album">
+          <span><img src="img/album.png" alt=""></span>
+          <h4>The dark side of the moon</h4>
+          <p>Pink Floyd</p>
+          <h5>1973</h5>
+        </div>
+        <div class="album">
+          <span><img src="img/album.png" alt=""></span>
+          <h4>The dark side of the moon</h4>
+          <p>Pink Floyd</p>
+          <h5>1973</h5>
+        </div>
+        <div class="album">
+          <span><img src="img/album.png" alt=""></span>
+          <h4>The dark side of the moon</h4>
+          <p>Pink Floyd</p>
+          <h5>1973</h5>
+        </div>
+        <div class="album">
+          <span><img src="img/album.png" alt=""></span>
+          <h4>The dark side of the moon</h4>
+          <p>Pink Floyd</p>
+          <h5>1973</h5>
+        </div>
+        <div class="album">
+          <span><img src="img/album.png" alt=""></span>
+          <h4>The dark side of the moon</h4>
+          <p>Pink Floyd</p>
+          <h5>1973</h5>
+        </div>
+      </main>
+    </div>
+
+  </body>
+</html>
---
+++ script.js
@@ -0,0 +1,28 @@
+// Stampare a schermo una decina di dischi musicali
+// (vedi screenshot) in due modi diversi:
+// - Solo con l'utilizzo di PHP, che stampa
+// direttamente i dischi in pagina: al caricamento
+// della pagina ci saranno tutti i dischi.
+// - Attraverso l'utilizzo di AJAX: al caricamento
+// della pagina ajax chiederà attraverso una
+// chiamata i dischi a php e li stamperà
+// attraverso handlebars.
+
+$(document).ready(function(){
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
